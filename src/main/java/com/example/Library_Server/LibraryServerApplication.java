a8ef83dbb92bef7a87ec8f232b8b5daf2c9a646b
FLTOSHI <fltoshimyname@gmail.com>
2024-03-01 11:13:51 +0300

    01.03

--- src/main/java/com/example/Library_Server/LibraryServerApplication.java
+++ src/main/java/com/example/Library_Server/LibraryServerApplication.java
@@ -2,9 +2,8 @@ package com.example.Library_Server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class LibraryServerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/Library_Server/controller/BookController.java
+++ src/main/java/com/example/Library_Server/controller/BookController.java
@@ -4,7 +4,6 @@ import com.example.Library_Server.entity.BookEntity;
 import com.example.Library_Server.response.BaseResponse;
 import com.example.Library_Server.response.BookListResponse;
 import com.example.Library_Server.service.BookService;
-import com.fasterxml.jackson.databind.ser.Serializers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,12 +25,12 @@ public class BookController {
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<?> update(@RequestBody BookEntity book, @PathVariable Long id) {
+    public ResponseEntity<BaseResponse> update(@RequestBody BookEntity book, @PathVariable Long id) {
         try {
             service.save(book);
             return ResponseEntity.ok(new BaseResponse(true, "Поле изменено"));
         } catch (Exception e) {
-            return ResponseEntity.ok(new BaseResponse(false, "шо-то пошлоу не так."));
+            return ResponseEntity.ok(new BaseResponse(false, "Што-то пошлоу не так."));
         }
     }
 
@@ -42,6 +41,19 @@ public class BookController {
             return ResponseEntity.ok(new BaseResponse(true, "Книга добавлена"));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(new BaseResponse(false, e.getMessage()));
+        }
+    }
+
+    @DeleteMapping ("/delete")
+    public ResponseEntity <BaseResponse> delete(@RequestBody BookEntity data){
+        try{
+            if (data.getId()==null)
+                throw new RuntimeException("Такой книги нет, и удалять я ничего не буду.");
+
+            service.delete(data.getId());
+            return ResponseEntity.ok(new BaseResponse(true,"Книга удалёна"));
+        }catch (Exception e) {
+            return ResponseEntity.badRequest().body(new BaseResponse(false,e.getMessage()));
         }
     }
 }
